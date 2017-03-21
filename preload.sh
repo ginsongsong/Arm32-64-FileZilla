13269b47fe1d3fc57934cb71f59516d6d5dbe0d1
root <root@gin_smtp>
2017-03-21 23:29:50 +0800

    Clone from https://svn.filezilla-project.org/svn/FileZilla3/trunk add git from https://github.com/basvodde/filezilla/commits/master
    Add preload script and step for arm buiding filezilla excutable file

---
+++ preload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+  apt-get update
+  apt-get install automake -y
+  apt-get install libtoolize -y
+  apt-get install libtool -y
+  apt-get install wx2.8-headers libwxgtk2.8-0 libwxgtk2.8-dev -y
+  apt-get install sqlite3 libsqlite3-dev -y
+  apt-get install  libtinyxml-dev -y
+  apt-get install  libdbus-1-dev -y
+
